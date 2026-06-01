987c51fcf6165d1d71edb21d410a35c8f486e650
Shykat <shykat@co.design>
2026-06-01 16:15:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    title: "Sapphire",
+    status: "success",
+    message: "CI/CD learning!",
+    date: new Date(),
+  });
+});
+
+app.listen(3000, () => {
+  console.log("CI/CD learning server is running on port 3000");
+});
